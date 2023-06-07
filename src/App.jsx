39b2fff25383647dcffa8ec8871bb549036c1dc4
Silvio Ruiz <silviojrck@gmail.com>
2023-06-07 18:30:29 -0300

    Agrega maquetado del formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import ContenedorNoticias from './components/ContenedorNoticias';
 import Titulo from './components/Titulo';
+import Buscador from './components/Buscador';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Titulo content={ "Noticias" }></Titulo>
+      <Buscador></Buscador>
       <ContenedorNoticias></ContenedorNoticias>
     </>
   )
---
+++ src/components/Buscador.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Container, Form, Row, Col } from 'react-bootstrap';
+
+const Buscador = () => {
+    return (
+        <Container className='text-light mt-5 text-center'>
+            <Form>
+                <Form.Group as={Row}>
+                    <Form.Label column sm="10" md="4">
+                         Buscar una categoría:
+                    </Form.Label>
+                    <Col sm="10" md="8">
+                        <Form.Select>
+                            <option>Elija una opcion</option>
+                            <option>Economía</option>
+                            <option>Policiales</option>
+                            <option>Deportes</option>
+                        </Form.Select>
+                    </Col>
+                </Form.Group>
+            </Form>
+        </Container>
+    );
+};
+
+export default Buscador;
