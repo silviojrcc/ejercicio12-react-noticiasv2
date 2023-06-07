e689a0b5d2074a2b2fae83118e88a01f53c05214
Silvio Ruiz <silviojrck@gmail.com>
2023-06-07 16:46:35 -0300

    Cambia el nombre del proyecto y agrega componente contenedor noticias

--- src/components/ContenedorNoticias.jsx
+++ src/components/ContenedorNoticias.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Container } from 'react-bootstrap';
 
 const ContenedorNoticias = props => {
     return (
-        <div>
-            
-        </div>
+        <Container>
+            <section className='d-flex row justify-content-around mt-4 p-5'>
+                <h2>Hola</h2>
+            </section>
+        </Container>
     );
 };
 
