6e7447031403949e06782b1f1fc38ca59e284524
Silvio Ruiz <silviojrck@gmail.com>
2023-06-08 00:21:51 -0300

    Agrega funcionalidad de buscar noticias por categoria y pais

--- src/components/Buscador.jsx
+++ src/components/Buscador.jsx
@@ -1,25 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import { Container, Form, Row, Col, Spinner } from 'react-bootstrap';
 import ContenedorNoticias from './ContenedorNoticias';
-import { getNewsByCategory } from "../components/helpers/queries"
+import { getNewsByCategoryAndCountry } from "../components/helpers/queries"
 
 const Buscador = () => {
 
     const [category, setCategory] = useState("politics");
+    const [country, setCountry] = useState("ar");
     const [news, setNews] = useState([]);
     const [showSpinner, setShowSpinner] = useState(true);
 
     const handleCategoryChange = (event) => {
         setCategory(event.target.value);
-        console.log(category);
+    };
+
+    const handleCountryChange = (event) => {
+        setCountry(event.target.value);
     };
 
     useEffect(() => {
         const fetchData = async () => {
             try {
                 setShowSpinner(true);
-                const response = await getNewsByCategory(category);
+                const response = await getNewsByCategoryAndCountry(category, country);
                 setNews(response.results);
+                console.log(news);
                 setShowSpinner(false);
             } catch (err) {
                 setNews([]);
@@ -27,9 +32,9 @@ const Buscador = () => {
         };
       
         fetchData();
-    }, [category]);
+    }, [category, country]);
 
-      const showComponent = (showSpinner) ? (<div className="my-5"><Spinner animation="border" variant="primary" /></div>) : <ContenedorNoticias news={news}></ContenedorNoticias>;
+    const showComponent = (showSpinner) ? (<div className="my-5"><Spinner animation="border" variant="primary" /></div>) : <ContenedorNoticias news={news}></ContenedorNoticias>;
 
     return (
         <Container className='text-light mt-5 text-center'>
@@ -43,6 +48,21 @@ const Buscador = () => {
                             <option value={"politics"}>Politica</option>
                             <option value={"entertainment"}>Entretenimiento</option>
                             <option value={"sports"}>Deportes</option>
+                        </Form.Select>
+                    </Col>
+                </Form.Group>
+                <Form.Group as={Row} className='my-4'>
+                    <Form.Label column sm="10" md="4">
+                         Buscar por país:
+                    </Form.Label>
+                    <Col sm="10" md="6">
+                        <Form.Select onChange={ handleCountryChange }>
+                            <option value={"ar"}>Argentina</option>
+                            <option value={"br"}>Brasil</option>
+                            <option value={"uy"}>Uruguay</option>
+                            <option value={"cl"}>Chile</option>
+                            <option value={"bo"}>Bolivia</option>
+                            <option value={"uz"}>Uzbekistan</option>
                         </Form.Select>
                     </Col>
                 </Form.Group>
--- src/components/helpers/queries.js
+++ src/components/helpers/queries.js
@@ -1,7 +1,8 @@
-const API_KEY =  import.meta.env.VITE_API_KEY
+// const API_KEY =  import.meta.env.VITE_API_KEY
+const API_KEY =  "pub_241775ba736573459eb84da67935534ce47ff";
 
-export const getNewsByCategory = async(category) => {
-    const url = `https://newsdata.io/api/1/news?apikey=${ API_KEY }&category=${ category }&country=ar`;
+export const getNewsByCategoryAndCountry = async(category, country) => {
+    const url = `https://newsdata.io/api/1/news?apikey=${ API_KEY }&category=${ category }&country=${ country }`;
     try{
         const response = await fetch(url);
         const data = await response.json();
