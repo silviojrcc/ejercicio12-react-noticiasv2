1bad60f07be9dfe66b9d60213422cb01d7de3fda
Silvio Ruiz <silviojrck@gmail.com>
2023-06-08 00:44:56 -0300

    Cambios en queries.js

--- src/components/helpers/queries.js
+++ src/components/helpers/queries.js
@@ -2,7 +2,6 @@ const apiKey = import.meta.env.VITE_API_KEY;
 
 export const getNewsByCategoryAndCountry = async(category, country) => {
     const url = `https://newsdata.io/api/1/news?apikey=${ apiKey }&category=${ category }&country=${ country }`;
-
     try{
         const response = await fetch(url);
         const data = await response.json();
