655af08bf2e6ebc170178f6be01f74b889365abf
Silvio Ruiz <silviojrck@gmail.com>
2023-06-07 22:20:22 -0300

    Sacando un componente container

--- src/components/ContenedorNoticias.jsx
+++ src/components/ContenedorNoticias.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Container } from 'react-bootstrap';
 import Noticia from './Noticia';
 
 const ContenedorNoticias = ({ news }) => {
     return (
-        <Container>
-            <section className='d-flex row justify-content-around mt-4 p-5'>
-                {news.map(noticia => <Noticia key={noticia.title} noticia={noticia} />)}
-            </section>
-        </Container>
+        <section className='d-flex row justify-content-around mt-4 p-5'>
+            {news.map(noticia => <Noticia key={noticia.title} noticia={noticia} />)}
+        </section>
     );
 };
 
