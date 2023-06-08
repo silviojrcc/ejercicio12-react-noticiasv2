2a69757459004d72378d44a876c5d5d313929d17
Silvio Ruiz <silviojrck@gmail.com>
2023-06-07 21:44:48 -0300

    Agrega proptypes al componente contenedor noticias

--- src/components/ContenedorNoticias.jsx
+++ src/components/ContenedorNoticias.jsx
@@ -14,7 +14,23 @@ const ContenedorNoticias = ({ news }) => {
 };
 
 ContenedorNoticias.propTypes = {
-    
+    news: PropTypes.arrayOf(
+        PropTypes.shape({
+            title: PropTypes.string.isRequired,
+            link: PropTypes.string.isRequired,
+            keywords: PropTypes.arrayOf(PropTypes.string),
+            creator: PropTypes.arrayOf(PropTypes.string),
+            video_url: PropTypes.string,
+            description: PropTypes.string,
+            content: PropTypes.string,
+            pubDate: PropTypes.string.isRequired,
+            image_url: PropTypes.string,
+            source_id: PropTypes.string.isRequired,
+            category: PropTypes.arrayOf(PropTypes.string),
+            country: PropTypes.arrayOf(PropTypes.string),
+            language: PropTypes.string.isRequired,
+        })
+    ).isRequired,
 };
 
 export default ContenedorNoticias;
